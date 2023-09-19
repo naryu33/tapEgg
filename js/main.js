eb106012e4942ac712cb69bc9dea778c67d2ed9c
ray-chan-dayo <rayignis@gmail.com>
2023-09-19 18:22:51 +0900

    This egg has never been cracked!

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const intialValue = 1000000;
+const countText = document.getElementById('count');
+const eggImg = document.getElementById('egg-img');
+const tapSound = document.getElementById('tap-sound');
+let count = localStorage.getItem('tapCount');
+
+if (!localStorage.getItem('tapCount')) {
+    count = intialValue;
+}
+countText.textContent = count;
+eggImg.onclick = function() {
+    count--;
+    localStorage.setItem('tapCount', count);
+
+    if (count <= 0) {
+        countText.textContent = 'ワレルトオモウナヨ';
+        this.src = 'img/cracked-egg.png';
+        localStorage.removeItem('tapCount');
+        return;
+    }
+
+
+    countText.textContent = count;
+    tapSound.currentTime = 0;
+    tapSound.play();
+    this.classList.add('egg-tap');
+    setTimeout(function() {
+        eggImg.classList.remove('egg-tap');
+    }, 200);
+};
