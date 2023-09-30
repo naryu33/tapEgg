de4d9de06b32b4c8ca6f88d6e8633d55c041f6a0
naryu33 <145438564+naryu33@users.noreply.github.com>
2023-09-30 16:59:57 +0900

    ひよこ機能追加

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const countText = document.getElementById('count');
 const eggImg = document.getElementById('egg-img');
 const tapSound = document.getElementById('tap-sound');
 let count = localStorage.getItem('tapCount');
+let chick = localStorage.getItem('chickCount');
 
 if (!localStorage.getItem('tapCount')) {
     count = intialValue;
@@ -14,9 +15,11 @@ eggImg.onclick = function() {
 
     if (count <= 0) {
         if (count == 0) {
-            if (Math.floor(Math.random()*10)==5) {
+            if (Math.floor(Math.random()*2)==1) {
                 countText.textContent = 'ワレチャッタネ、ザンネン';
                 this.src = 'img/hiyoko_baby.png';
+                chick++
+                localStorage.setItem('chickCount', chick);
                 
             } else {
                 countText.textContent = 'ワレルトオモウナヨ';
@@ -36,4 +39,14 @@ eggImg.onclick = function() {
         eggImg.classList.remove('egg-tap');
     }, 200);
 };
+setInterval(() => {
+    if (chick) {
+        for (let index = 0; index < chick; index++) {
+            eggImg.onclick()    
+            
+        }
+            
+    }
+    
+}, 600);
 alert("この卵を割ってみて下さい")
